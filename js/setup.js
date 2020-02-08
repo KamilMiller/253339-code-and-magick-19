72085d461bb80968288a083e75f910f249e26a38
Slow-match <boarsfield@gmail.com>
2020-02-08 18:27:17 +0300

    Учебный проект: нас орда

--- js/setup.js
+++ js/setup.js
@@ -1,12 +1,12 @@
 'use strict';
 
 // Число магов.
-var NUMBER_OF_WIZARDS = 4;
+var WIZARDS_COUNT = 4;
 
 // Включение вдимости элемента .setup и .setup-similar.
 var setupElement = document.querySelector('.setup');
-setupElement.classList.remove('hidden');
 var setupSimilarBlock = document.querySelector('.setup-similar');
+setupElement.classList.remove('hidden');
 setupSimilarBlock.classList.remove('hidden');
 
 // Массив с подмассивами данных о персонаже игры.
@@ -17,9 +17,6 @@ var WizardLibrary = {
   EYES_COLOR: ['black', 'red', 'blue', 'yellow', 'green']
 };
 
-// Пустой массив.
-var wizards = [];
-
 // Функция получения случайных целых чисел.
 var getRandomInt = function (min, max) {
   min = Math.ceil(min);
@@ -27,46 +24,50 @@ var getRandomInt = function (min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
+// Переменная для хранения индекса в массиве.
+var elementIndex;
+
+// Функция случайного изъятия элемента из массива.
+var spliceRandom = function (arr, index) {
+  elementIndex = index;
+  var randomElement = arr[elementIndex];
+  arr.splice(elementIndex, 1);
+  return randomElement;
+};
+
 // Функция сборки данных магов.
-var wizardDataAssembling = function (quantity) {
+var mockData = function (quantity) {
   var WizardLibraryCopy = {};
-  WizardLibraryCopy.NAME = WizardLibrary.NAME.slice();
-  WizardLibraryCopy.SURNAME = WizardLibrary.SURNAME.slice();
-  WizardLibraryCopy.COAT_COLOR = WizardLibrary.COAT_COLOR.slice();
-  WizardLibraryCopy.EYES_COLOR = WizardLibrary.EYES_COLOR.slice();
+  var wizards = [];
+  WizardLibraryCopy.names = WizardLibrary.NAME.slice();
+  WizardLibraryCopy.surnames = WizardLibrary.SURNAME.slice();
+  WizardLibraryCopy.coatColors = WizardLibrary.COAT_COLOR.slice();
+  WizardLibraryCopy.eyesColors = WizardLibrary.EYES_COLOR.slice();
   for (var i = 0; i < quantity; i++) {
-    var nameIndex = getRandomInt(0, WizardLibraryCopy.NAME.length - 1);
-    var coatColorIndex = getRandomInt(0, WizardLibraryCopy.COAT_COLOR.length - 1);
-    var eyesColorIndex = getRandomInt(0, WizardLibraryCopy.EYES_COLOR.length - 1);
     wizards.push({
-      name: WizardLibraryCopy.NAME[nameIndex],
-      surname: WizardLibraryCopy.SURNAME[nameIndex],
-      coatColor: WizardLibraryCopy.COAT_COLOR[coatColorIndex],
-      eyesColor: WizardLibraryCopy.EYES_COLOR[eyesColorIndex]
+      name: spliceRandom(WizardLibraryCopy.names, getRandomInt(0, WizardLibraryCopy.names.length - 1)),
+      surname: spliceRandom(WizardLibraryCopy.surnames, elementIndex),
+      coatColor: spliceRandom(WizardLibraryCopy.coatColors, getRandomInt(0, WizardLibraryCopy.coatColors.length - 1)),
+      eyesColor: spliceRandom(WizardLibraryCopy.eyesColors, getRandomInt(0, WizardLibraryCopy.eyesColors.length - 1))
     });
-    WizardLibraryCopy.NAME.splice(nameIndex, 1);
-    WizardLibraryCopy.SURNAME.splice(nameIndex, 1);
-    WizardLibraryCopy.COAT_COLOR.splice(coatColorIndex, 1);
-    WizardLibraryCopy.EYES_COLOR.splice(eyesColorIndex, 1);
   }
-  return wizardDataAssembling;
+  return wizards;
 };
 
 // Функция сборки магов.
 var wizardElementAssembling = function () {
-  wizardDataAssembling(NUMBER_OF_WIZARDS);
+  var wizards = mockData(WIZARDS_COUNT);
   var LIST_OF_WIZARDS = document.querySelector('.setup-similar-list');
   var fragment = document.createDocumentFragment();
   var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
-  for (var i = 1; i <= NUMBER_OF_WIZARDS; i++) {
+  wizards.forEach(function (wizard) {
     var similarWizardItem = similarWizardTemplate.cloneNode(true);
-    similarWizardItem.querySelector('.setup-similar-label').textContent = wizards[i - 1].name + ' ' + wizards[i - 1].surname;
-    similarWizardItem.querySelector('.wizard-coat').style.fill = wizards[i - 1].coatColor;
-    similarWizardItem.querySelector('.wizard-eyes').style.fill = wizards[i - 1].eyesColor;
+    similarWizardItem.querySelector('.setup-similar-label').textContent = wizard.name + ' ' + wizard.surname;
+    similarWizardItem.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+    similarWizardItem.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
     fragment.appendChild(similarWizardItem);
-  }
+  });
   LIST_OF_WIZARDS.appendChild(fragment);
-  return wizardElementAssembling;
 };
 
 // Запуск функции сборки магов.
