3b77a40b5febabf014722fcc65ab7444218ffa30
Slow-match <boarsfield@gmail.com>
2020-02-17 23:27:58 +0300

    Учебный проект: одеть Надежду

--- js/setup.js
+++ js/setup.js
@@ -1,34 +1,43 @@
 'use strict';
 
-// Константы.
+// 1. Константы.
 var WIZARDS_COUNT = 4;
+var ESC_KEY = 'Escape';
+var ENTER_KEY = 'Enter';
 var LIST_OF_WIZARDS = document.querySelector('.setup-similar-list');
 var SIMILAR_WIZARD_TEMPLATE = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
+var SETUP_OPEN = document.querySelector('.setup-open');
+var SETUP = document.querySelector('.setup');
+var SETUP_CLOSE = SETUP.querySelector('.setup-close');
+// var SETUP_SIMILAR = SETUP.querySelector('.setup-similar');
+var SETUP_USER_NAME = SETUP.querySelector('.setup-user-name');
+var WIZARD_COAT = SETUP.querySelector('.wizard-coat');
+var WIZARD_EYES = SETUP.querySelector('.wizard-eyes');
+var SETUP_FIREBALL_WRAP = SETUP.querySelector('.setup-fireball-wrap');
+var COAT_INPUT = SETUP.querySelector('input[name=coat-color]');
+var EYES_INPUT = SETUP.querySelector('input[name=eyes-color]');
+var FIREBALL_INPUT = SETUP.querySelector('input[name=fireball-color]');
 
-// Скрытые элементы.
-var setupElement = document.querySelector('.setup');
-var setupSimilarBlock = document.querySelector('.setup-similar');
-
-
-// Объект с массивами данных о персонаже игры.
+// 2. Объект с массивами данных о персонаже игры.
 var WizardLibrary = {
   NAME: ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'],
   SURNAME: ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'],
   COAT_COLOR: ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'],
-  EYES_COLOR: ['black', 'red', 'blue', 'yellow', 'green']
+  EYES_COLOR: ['black', 'red', 'blue', 'yellow', 'green'],
+  FIREBALLS: ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848']
 };
 
-// Функция получения случайных целых чисел.
+// 3. Функция получения случайных целых чисел.
 var getRandomInt = function (min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-// Переменная для хранения индекса в массиве.
+// 4. Переменная для хранения индекса в массиве.
 var elementIndex;
 
-// Функция случайного изъятия элемента из массива.
+// 5. Функция случайного изъятия элемента из массива.
 var spliceRandom = function (arr) {
   elementIndex = getRandomInt(0, arr.length - 1);
   var randomElement = arr[elementIndex];
@@ -36,7 +45,7 @@ var spliceRandom = function (arr) {
   return randomElement;
 };
 
-// Функция сборки данных магов.
+// 6. Функция сборки данных магов.
 var mockData = function (quantity) {
   var wizards = [];
   var names = WizardLibrary.NAME.slice();
@@ -54,7 +63,7 @@ var mockData = function (quantity) {
   return wizards;
 };
 
-// Функция сборки магов.
+// 7. Функция сборки магов.
 var renderWizards = function (data) {
   var fragment = document.createDocumentFragment();
   data.forEach(function (wizard) {
@@ -67,7 +76,85 @@ var renderWizards = function (data) {
   return fragment;
 };
 
-// Раскрытие элементо ви запуск функции сборки магов.
-setupElement.classList.remove('hidden');
-setupSimilarBlock.classList.remove('hidden');
+// 8. Запуск функции сборки магов.
 LIST_OF_WIZARDS.appendChild(renderWizards(mockData(WIZARDS_COUNT)));
+
+// 9. Открытие/закрытие окна выбора персонажа.
+var onSetupEscPress = function (evt) {
+  if (evt.key === ESC_KEY) {
+    closeSetup();
+  }
+};
+
+var openSetup = function () {
+  SETUP.classList.remove('hidden');
+  document.addEventListener('keydown', onSetupEscPress);
+};
+
+var closeSetup = function () {
+  if (SETUP_USER_NAME !== document.activeElement) {
+    SETUP.classList.add('hidden');
+    document.removeEventListener('keydown', onSetupEscPress);
+  }
+};
+
+SETUP_OPEN.addEventListener('click', function () {
+  openSetup();
+});
+
+SETUP_OPEN.addEventListener('keydown', function (evt) {
+  if (evt.key === ENTER_KEY) {
+    openSetup();
+  }
+});
+
+SETUP_CLOSE.addEventListener('click', function () {
+  closeSetup();
+});
+
+SETUP_CLOSE.addEventListener('keydown', function (evt) {
+  if (evt.key === ENTER_KEY) {
+    closeSetup();
+  }
+});
+
+// 10. Изменение внешнего вида персонажа.
+// 10.1. Мантия.
+var coatColorsCopy = WizardLibrary.COAT_COLOR.slice();
+var eyesColorCopy = WizardLibrary.EYES_COLOR.slice();
+var fireBallsCopy = WizardLibrary.FIREBALLS.slice();
+
+var getNextColor = function (arr, element) {
+  var currentItem = arr.shift();
+  arr.push(currentItem);
+  element.style = 'fill: ' + arr[0];
+  return arr[0];
+};
+
+WIZARD_COAT.addEventListener('click', function () {
+  // getNextColor(coatColorsCopy, WIZARD_COAT);
+  COAT_INPUT.value = getNextColor(coatColorsCopy, WIZARD_COAT);
+});
+
+WIZARD_EYES.addEventListener('click', function () {
+  // getNextColor(eyesColorCopy, WIZARD_EYES);
+  EYES_INPUT.value = getNextColor(eyesColorCopy, WIZARD_EYES);
+});
+
+SETUP_FIREBALL_WRAP.addEventListener('click', function () {
+  FIREBALL_INPUT.value = getNextColor(fireBallsCopy, SETUP_FIREBALL_WRAP);
+  SETUP_FIREBALL_WRAP.style = 'background: ' + FIREBALL_INPUT.value;
+});
+
+// Вадидакция формы.
+SETUP_USER_NAME.addEventListener('invalid', function () {
+  if (SETUP_USER_NAME.validity.tooShort) {
+    SETUP_USER_NAME.setCustomValidity('Имя должно состоять минимум из 2-х символов');
+  } else if (SETUP_USER_NAME.validity.tooLong) {
+    SETUP_USER_NAME.setCustomValidity('Имя не должно превышать 25-ти символов');
+  } else if (SETUP_USER_NAME.validity.valueMissing) {
+    SETUP_USER_NAME.setCustomValidity('Обязательное поле');
+  } else {
+    SETUP_USER_NAME.setCustomValidity('');
+  }
+});
